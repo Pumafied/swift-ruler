b55bb97b4a9afef0c06ad3a92681fe1de0020cb8
Austin Luber <austin@lubkerdesign.com>
2016-10-18 20:08:29 -0500

    completes 7

--- euler-7.swift
+++ euler-7.swift
@@ -5,35 +5,28 @@ var compareArray = [2]
 var primeCheck:Int = 0
 
 func isPrime(number:Int)->Bool{
-	primeCheck = number
+	primeCheck = number - 1
 		while(primeCheck > 1){
 			//print("PrimeChecking")
 
-				if(number % primeCheck != 0){
+				if(number % primeCheck == 0){
 					return false
 
 				}
 			primeCheck = primeCheck - 1
 		}
-	print("is Prime")
+	//print("is Prime")
 	return true
 
 }
 	//This version of is prime relies on a store and compare version
 	func fasterIsPrime(number:Int)->Bool{
-		print("number " + String(number))
-		print("number in array " + String(compareArray[compareArray.count-1]))
-		print("array index " + String(compareArray.count-1))
 		if(number > compareArray[compareArray.count - 1]){
-			print("number Greater")
 			//Generate more arrays until the bottom
 			var check:Int = compareArray[compareArray.count - 1] + 1
 			//Make sure it ends up where it needs to be
-			while(check < number){
-				//print("checking")
-
-				if(isPrime(check)){
-					print("is Prim")
+			while(check <= number){
+				if(isPrime(number:check)){
 					compareArray.append(check)
 				}
 				if(check > number){
@@ -42,10 +35,8 @@ func isPrime(number:Int)->Bool{
 				check = check + 1
 			}
 		}
-		print("At the else")
 		//Implicit else - plus added benifit ofsolving after adding
 		//Figure out if the current number is in the array
-		print(compareArray.contains(number))
 		return compareArray.contains(number)
 
 	}
@@ -54,13 +45,15 @@ var current:Int = 2
 var primeOrder:Int = 0
 
 while true{
-	print("current" + String(current))
-	if(fasterIsPrime(current)){
+	if(fasterIsPrime(number:current)){
 		primeOrder += 1
+		//print(primeOrder)
 	}
 
-	if(primeOrder == 100001){
-		print("answer" + String(current))
+	//Itd is 0 based plus
+	if(compareArray.count == 10001){
+		print("Answer: " + String(compareArray[compareArray.count-1]))
+		break
 
 	}
 	current += 1
